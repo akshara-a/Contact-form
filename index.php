ed1ed7350ce03a008cdf7ba7d6f180abcd743463
akshara-a <68069921+akshara-a@users.noreply.github.com>
2021-04-24 16:19:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-    if (!empty($_POST)){
+    if ($_SERVER["REQUEST_METHOD"]=="POST"){
         $name = $_POST["name"]; 
         $email = $_POST["email"]; 
-        $message = $_POST["message"]; 
-        
-        $to = "akshararajan1726@gmail.com";
+        $message = $_POST["message"];
+        $to = 'akshararajan1726@gmail.com';
+        $from = $_POST['email'];
         $email_subject = "Contact form submission: $name";
         $email_body = "You have received a new message. ". " Here are the details:\n Name: $name \n ". "Email: $email\n Message \n $message";
-        $headers= "Reply-To: $email";
+        $headers= "Reply-To: $from";
         mail($to,$email_subject,$email_body,$headers);
     }
 
