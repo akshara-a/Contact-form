782d50b9ab4afa21fd33bb39933b452cea55cf09
akshara-a <68069921+akshara-a@users.noreply.github.com>
2021-04-25 19:26:29 +0530

    Delete Contact form with both mail storing and database storing feature directory

--- Contact form with both mail storing and database storing feature/db.php
+++
@@ -1,8 +0,0 @@
-<?php
- $host = "localhost";  
- $db_user = "root";  
- $db_pass = "";  
- $dbname = "adoption";  
-
- $connect = new PDO("mysql:host=$host; dbname=$dbname", $db_user, $db_pass); 
-?>
