3a277beff9e989e7ae79ff8b54e21e6be9713d8e
akshara-a <68069921+akshara-a@users.noreply.github.com>
2021-04-25 19:29:40 +0530

    Update index.php

--- Contact form with mail sending functionality/index.php
+++ Contact form with mail sending functionality/index.php
@@ -9,8 +9,6 @@
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-    <!-- JQUERY CDN -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <style>
         form{
             margin: 50px;
@@ -58,10 +56,11 @@
         $name = $_POST["name"]; 
         $email = $_POST["email"]; 
         $message = $_POST["message"];
-        $to="yourmail@gmail.com"; //Enter your email
+        $to="example@gmail.com"; //Enter your email
         $email_subject = "Contact form submission: $name";
         $email_body = "You have received a new message. ". " Here are the details:\n Name: $name \n ". "Email: $email\n Message \n $message";
-        $headers= "Reply-To: $email";
+        $headers = "From: website@domain.com"; //Enter the from email
+        $headers.= "Reply-To: $email";
         if(mail($to,$email_subject,$email_body,$headers)){
             echo '<div class="success"> Success! </div>';
         }
